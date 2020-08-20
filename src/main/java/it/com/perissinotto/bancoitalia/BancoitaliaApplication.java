1675a209cc58d60202f5869236d3e927fa87e0f4
Alexandre Perissinotto <68646755+aperissinotto@users.noreply.github.com>
2020-08-19 21:13:57 -0300

    Add files via upload

---
+++ src/main/java/it/com/perissinotto/bancoitalia/BancoitaliaApplication.java
@@ -0,0 +1,13 @@
+package it.com.perissinotto.bancoitalia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancoitaliaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BancoitaliaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/com/perissinotto/bancoitalia/BancoitaliaApplicationTests.java
@@ -0,0 +1,13 @@
+package it.com.perissinotto.bancoitalia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BancoitaliaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
